7411f6eebd5325ea0f80c78268b27a7d47d6474e
SHRESTHGUPTA106 <shresthgupta106@gmail.com>
2022-12-08 14:29:56 +0530

    Final changes

--- data.js
+++ data.js
@@ -5,34 +5,35 @@ const college = require('./dataSchema');
 const mongoose = require('mongoose');
 const { response } = require('express');
 
-// app.use(cors());
-
-
-router.post("/",(req,res,next)=>{
-    const data = new college({
-    _id:new mongoose.Types.ObjectId,
-    Name:req.body.Name,
-    Dept:req.body.Dept,
-    Location:req.body.Location,
-    Website:req.body.Website,
-    Autonomy_status:req.body.Autonomy_status,
-    College_Status:req.body.College_Status,
-    HostelAvailability:req.body.HostelAvailability
-    })
-    data.save()
-    .then(result=>{
-        console.log(result);
-        res.status(200).json({
-            college : result
-        })
-    })
-    .catch(err=>{
-        console.log('error');
-        res.status(500).json({
-            error:err
-        })
-    })
+router.get("/",(req,res)=>{
+    res.send({status:"Running"})
 })
+
+// router.post("/",(req,res,next)=>{
+//     const data = new college({
+//     _id:new mongoose.Types.ObjectId,
+//     Name:req.body.Name,
+//     Dept:req.body.Dept,
+//     Location:req.body.Location,
+//     Website:req.body.Website,
+//     Autonomy_status:req.body.Autonomy_status,
+//     College_Status:req.body.College_Status,
+//     HostelAvailability:req.body.HostelAvailability
+//     })
+//     data.save()
+//     .then(result=>{
+//         console.log(result);
+//         res.status(200).json({
+//             college : result
+//         })
+//     })
+//     .catch(err=>{
+//         console.log('error');
+//         res.status(500).json({
+//             error:err
+//         })
+//     })
+// })
 
 router.get("/colleges", async (req,res,next)=>{
     let College = await college.find();
